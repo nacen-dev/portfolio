faa51a9aab17d4d3ddb809fb6935ea92f8e20a83
Vincent <nacen.dev@gmail.com>
2022-06-16 22:44:22 +0800

    style: add blue bottom border if route matches

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,21 +1,48 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface Props {}
 
 export const Header = (props: Props) => {
+  const router = useRouter();
+
   return (
-    <header className="p-5 flex justify-center">
+    <header className="p-5 flex justify-center h-[10vh]">
       <nav>
-        <ul className="flex gap-4 text-2xl p-2">
+        <ul className="flex gap-4 text-2xl">
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/">
+              <a
+                className={`${
+                  router.pathname == "/" ? "border-b-2 border-blue-300" : ""
+                }`}
+              >
+                Home
+              </a>
+            </Link>
           </li>
           <li>
-            <Link href="/projects">Projects</Link>
+            <Link href="/projects">
+              <a
+                className={`${
+                  router.pathname == "/projects" ? "border-b-2 border-blue-300" : ""
+                }`}
+              >
+                Projects
+              </a>
+            </Link>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <Link href="/contact">
+              <a
+                className={`${
+                  router.pathname == "/contact" ? "border-b-2 border-blue-300" : ""
+                }`}
+              >
+                Contact
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
