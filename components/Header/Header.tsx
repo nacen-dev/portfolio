729ebdc506f135b861f899ee9c516ad9d9c22c16
Vincent <nacen.dev@gmail.com>
2022-06-17 20:01:51 +0800

    style: change background, check for undefined

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,15 +8,15 @@ export const Header = (props: Props) => {
   const router = useRouter();
 
   return (
-    <header className="p-5 flex justify-center h-[10vh] sticky top-0 z-10 w-full bg-white">
+    <header className="p-5 flex justify-center h-[10vh] sticky top-0 z-10 w-full text-white bg-slate-800">
       <nav>
         <ul className="flex gap-4 text-2xl font-PlayFairDisplay">
           <li>
             <Link href="/">
               <a
                 className={`${
-                  router.pathname == "/" ? "border-b-2 border-cyan-800" : ""
-                } hover:text-sky-800`}
+                  router?.pathname == "/" ? "border-b-2 border-cyan-200 text-cyan-200" : ""
+                } hover:text-`}
               >
                 Home
               </a>
@@ -26,8 +26,8 @@ export const Header = (props: Props) => {
             <Link href="/projects">
               <a
                 className={`${
-                  router.pathname == "/projects" ? "border-b-2 border-cyan-800" : ""
-                } hover:text-sky-800`}
+                  router?.pathname == "/projects" ? "border-b-2 border-cyan-200 text-cyan-200" : ""
+                } hover:text-`}
               >
                 Projects
               </a>
@@ -37,8 +37,8 @@ export const Header = (props: Props) => {
             <Link href="/contact">
               <a
                 className={`${
-                  router.pathname == "/contact" ? "border-b-2 border-cyan-800" : ""
-                } hover:text-sky-800`}
+                  router?.pathname == "/contact" ? "border-b-2 border-cyan-200 text-cyan-200" : ""
+                } hover:text-`}
               >
                 Contact
               </a>
