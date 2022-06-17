a0131c6170d9056ae952c15e1108da70d187abab
Vincent <nacen.dev@gmail.com>
2022-06-17 20:52:58 +0800

    refactor: add contact me button

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,23 @@ interface Props {
 
 const Home: NextPage<Props> = ({ about }) => {
   return (
-    <main className="bg-sky-800 h-[90vh] p-4">
-      <div className="rounded bg-white p-4 py-6 sm:p-10 mt-10 lg:w-[70%] lg:mr-auto lg:ml-auto">
+    <main className="bg-slate-800 h-[90vh] p-4">
+      <div className="rounded text-white border-cyan-200 border-2 p-4 py-6 sm:p-10 mt-10 max-w-[768px] mx-4 sm:mr-auto sm:ml-auto">
         <h1 className="text-3xl sm:text-4xl font-PlayFairDisplay font-bold mb-4">
           {about.introduction}
         </h1>
-        <p className="text-xl">{about.description}</p>
-        <div className="flex justify-end w-full mt-4">
-          <button className="bg-sky-800 px-5 py-2 text-white rounded flex items-center gap-2 hover:scale-105 text-lg">
-            <MdArrowForwardIos />
+        <p className="text-xl text-slate-200">{about.description}</p>
+        <div className="flex sm:justify-end w-full mt-4">
+          <button className="bg-sky-800 px-5 py-2 text-white rounded flex items-center gap-2 hover:scale-105 text-lg max-w-[200px] w-full justify-center">
             <Link href="/projects">
               <a>View my work</a>
+            </Link>
+          </button>
+        </div>
+        <div className="flex sm:justify-end w-full mt-4">
+          <button className="bg-sky-800 px-5 py-2 text-white rounded flex items-center gap-2 hover:scale-105 text-lg max-w-[200px] w-full justify-center">
+            <Link href="/contact">
+              <a>Contact Me</a>
             </Link>
           </button>
         </div>
