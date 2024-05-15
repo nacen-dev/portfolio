28f7c62416dca8cab4bab4b7fa37ccf70ae6ee3d
Vincent <nacen.dev@gmail.com>
2024-05-15 14:36:27 +0800

    Update contact styling

--- components/Contact/contact.tsx
+++ components/Contact/contact.tsx
@@ -8,14 +8,12 @@ interface ContactProps {
 export const Contact = ({ contact }: ContactProps) => {
   return (
     <div
-      className={`max-w-[675px] mr-auto ml-auto p-8 border-2 border-cyan-200 rounded text-white`}
+      className={`max-w-[675px] mr-auto ml-auto px-6 py-4 md:px-8 border-2 border-cyan-200 rounded text-white`}
     >
       <div className="text-2xl my-5 flex flex-col gap-4 text-slate-200">
         <p>I&apos;m open to freelance opportunities or remote position.</p>
-        <p>
-          Have a question or want to collaborate? Contact me on one of my
-          socials below
-        </p>
+        <p>Have a question or want to collaborate?</p>
+        <p>Contact me on one of my socials below</p>
       </div>
 
       <Social contact={contact} className="flex gap-2 flex-wrap" />
